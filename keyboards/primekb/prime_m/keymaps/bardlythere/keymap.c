90e2048e40de4576d62e1ce326a645ebe05c0c2e
BardlyThere <50787530+BardlyThere@users.noreply.github.com>
2019-09-25 10:58:46 -0500

    Update keymap.c

--- keyboards/primekb/prime_m/keymaps/bardlythere/keymap.c
+++ keyboards/primekb/prime_m/keymaps/bardlythere/keymap.c
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   	MO(_EX), _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
   	_______, _______, KC_P4,   KC_P5,   KC_P6,   XXXXXXX,
   	KC_CALC, _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-  	_______, _______, KC_P0,   QM_00,   KC_PDOT, XXXXXXX
+  	_______, C(S(KC_N)), KC_P0,   QM_00,   KC_PDOT, XXXXXXX
     ),
 
   [_EX] = LAYOUT_ortho_5x6(
